30840d80c07ee4093158cb5017f576cc591d49fb
Car <91814466+Wongcarl198@users.noreply.github.com>
2024-03-06 15:49:18 -0700

    prompt, prompt double, DisplayProgramintro, displayMainMenu methods

--- Assignment_3/Program.cs
+++ Assignment_3/Program.cs
@@ -1,2 +1,68 @@
-﻿
-Console.WriteLine("im him, World!");
+﻿// <summary>
+// Assignment 3
+// 
+// Author: Carl Wong
+// Date: 2024-03-06
+// Purpose: Allows user to enter/save/load/edit/view daily sales values
+//          from a file. Allows and displays simple data analysis
+//          (mean/max/min/graph) of sales values for a given month.
+// </summary>
+//
+
+
+
+//Prompt method
+string Prompt(string promptString)
+{
+  string response = "";
+  Console.Write(promptString);
+  response = Console.ReadLine();
+  return response.ToUpper();
+}
+
+//PromptDouble method 
+double PromptDouble(string promptString)
+{
+    bool inValidInput = true;
+    double input = 0;
+    while(inValidInput)
+    {
+        try
+        {
+
+            Console.Write(promptString);
+            input = double.Parse(Console.ReadLine());
+            inValidInput = false;
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"InValid Input: {ex.Message}");
+        }
+    }
+    return input;
+}
+
+
+// Displays the Program intro.
+void DisplayProgramIntro()
+{
+	Console.WriteLine("========================================");
+	Console.WriteLine("=                                      =");
+	Console.WriteLine("=            Monthly  Sales            =");
+	Console.WriteLine("=                                      =");
+	Console.WriteLine("========================================");
+	Console.WriteLine();
+}
+
+void displayMainMenu()
+{
+    Console.WriteLine(" [N] New Daily Sales Entry");
+    Console.WriteLine(" [S] Save Entries to File");
+    Console.WriteLine(" [E] Edit Sales Entries");
+    Console.WriteLine(" [L] Load Sales File");
+    Console.WriteLine(" [V] View Entered/Loaded Sales");
+    Console.WriteLine(" [M] Montonly Statistics");
+    Console.WriteLine(" [D] Display Main Menu");
+    Console.WriteLine(" [Q] Quit Program");  
+}
+
