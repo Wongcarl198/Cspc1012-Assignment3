cd568ec468968e227c801a43cbc03b87d7e0c10d
Car <91814466+Wongcarl198@users.noreply.github.com>
2024-03-06 13:05:28 -0700

    update

--- Assignment_3/obj/Debug/net8.0/Assignment_3.AssemblyInfo.cs
+++ Assignment_3/obj/Debug/net8.0/Assignment_3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+952e9a22910c38732c2c2393b7f49196626c1b8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
